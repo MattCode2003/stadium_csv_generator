8c5c5a6051872440296f7f43e210df0f1f8a4567
MattCode2003 <Tkdmattpryke@outlook.com>
2026-04-12 20:08:40 +0100

    added: working with excel_decryption.exe

--- include/bucs.hpp
+++ include/bucs.hpp
@@ -3,4 +3,5 @@
 
 void bucs_menu();
 std::string pick_file();
-
+void bucs_excel_to_csv();
+void get_players(std::string file_path);
--- src/bucs.cpp
+++ src/bucs.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 #include "../include/loading_data.hpp"
 #include <nfd.hpp>
+#include <OpenXLSX.hpp>
+#include <filesystem>
+#include <cstdlib>
+
+namespace fs = std::filesystem;
 
 
 // menu
@@ -36,8 +41,7 @@ void bucs_menu()
             
             case 2:
             {
-                std::string file_path = pick_file();
-                std::cout << file_path;
+                bucs_excel_to_csv();
                 break;
             }
 
@@ -49,6 +53,25 @@ void bucs_menu()
                 break;
         }
     }
+}
+
+
+void bucs_excel_to_csv()
+{
+    // Gets file path
+    std::cout << "Select your player .xlsx file\n";
+    std::string player_excel_path = pick_file();
+
+    // Error handling
+    if (player_excel_path == "")
+    {
+        std::cout << "[Error] No file was selected.\n";
+        return;
+    }
+
+    // Gets player info and stores them in struct
+    get_players(player_excel_path);
+
 }
 
 
@@ -70,5 +93,55 @@ std::string pick_file()
 
     NFD::Quit();
     return "";
+
+}
+
+
+// Get player info and store in struct
+void get_players(std::string file_path)
+{
+    // Checks if the file is password protected or not
+    OpenXLSX::XLDocument doc;
+    int password_required = 0;
+    const fs::path temp_file_location = fs::temp_directory_path();
+    const std::string file_name = "players.xlsx";
+    
+    try
+    {
+        doc.open(file_path);
+    }
+
+    // file is encrypted and needs password
+    catch (const std::exception& e)
+    {
+        fs::path exe_path;
+        std::string password;
+
+        #if defined(_WIN32)
+            exe_path = fs::current_path() / "excel_decryption_win.exe";
+        #elif defined(__APPLE__)
+            exe_path = fs::current_path() / "excel_decryption_mac";
+        #elif defined(__linux__)
+            exe_path = fs::current_path() / "excel_decryption_linux";
+        #endif
+
+        std::cout << "Enter Password: ";
+        std::cin >> password;
+
+        std::string command = " \" \"" + exe_path.string() + "\" " +
+                      "\"" + file_path + "\" " +
+                      "\"" + password + "\" " +
+                      "\"" + (temp_file_location / "players.xlsx").string() + "\" \" ";
+        
+        std::system(command.c_str());
+
+        doc.open((temp_file_location / "players.xlsx").string());
+    }
+
+    // Gets all player info
+    auto wks = doc.workbook().worksheet(1);
+
+    uint32_t row_count = wks.rowCount();
+    uint32_t col_count = wks.columnCount();
 
 }
